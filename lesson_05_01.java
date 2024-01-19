0ad7d20db459cf5a8bcca80344348e9a36deed84
ColotVS <kolotilov98@mail.ru>
2024-01-19 13:12:48 +0700

    Лайфхаки IDEA Урок 5, задача 1

---
+++ lesson_05_01.java
@@ -0,0 +1,39 @@
+public class lesson_05_01 {
+    public static void main(String[] args) {
+        //JDK содержит JRE + инструменты для Java-разработчиков.
+
+        //JRE содержит JVM + набор стандартных Java-библиотек.
+
+        //JVM — это виртуальная машина Java.
+
+        //Автодополнение (Tab)
+
+        //public static void main(String[] args)
+        //Для этого нужно написать 4 буквы psvm и нажать Tab
+
+        //System.out.println();
+        //Для этого тоже нужно написать 4 буквы «sout» и нажать Tab
+
+        //public static final String
+        //Для этого тоже нужно написать 4 буквы «psfs» и нажать Tab
+
+        //for (int i = 0; i < ; i++) {}
+        //Быстро написать цикл в IDEA можно с помощью команды fori + Tab
+
+        //Оборачивание: Ctrl+Alt+T
+        //Выделить код, который вы хотите обернуть другим кодом
+        //Нажать Ctrl+Alt+T
+        //Выбрать один из вариантов оборачивания:
+        //if
+        //if-else
+        //while
+        //do while
+        //for
+        //try catch
+        //try finally
+        //try catch finally
+        //synchronized
+
+        //Стиль кода: Ctrl+Alt+L
+    }
+}
